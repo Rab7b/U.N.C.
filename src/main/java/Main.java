6b26d77426a0566e306c093d70ceec6567c2cfe8
Rab7b <5myinc5@gmail.com>
2026-03-30 20:35:44 -0300

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,11 +6,14 @@ public class Main {
         System.loadLibrary("neuron_logic");
     }
     public static void main(String[] args) {
-        Neuron neuron = new Neuron(2, 0.01, new double[]{0.5, 0.5}, new double[]{1}, 1);
-        for (int i = 0; i < 100000; i++) {
+        Neuron neuron = new Neuron(2, 0.01, new double[]{0.5, 0.5}, new double[]{1});
+        neuron.load("w.txt");
+        
+        for (int i = 0; i < 100; i++) {
             neuron.train(1);
-            System.out.println(neuron.predict());
+            neuron.motivate(i, new double[]{0.5, 0.5}, i);
         }
         System.out.println("Final answer: " + neuron.predict());
+        neuron.save("w.txt");
     }
 }
--- src/main/java/ai/Neuron.java
+++ src/main/java/ai/Neuron.java
@@ -1,7 +1,6 @@
 package ai;
 
 public class Neuron {
-
     private double[][] weights;
     private final double[] bias;
     private final double[] out;
@@ -9,32 +8,15 @@ public class Neuron {
     private double[] inputs;
     private double[] targets;
 
-    static {
-        System.loadLibrary("neuron_logic");
-    }
+    static { System.loadLibrary("neuron_logic"); }
 
-    public Neuron(int inputSize, double learningRate, double[] inputs, double[] targets, int outs) {
-        this.weights = new double[inputSize+3][7];
+    public Neuron(int inputSize, double learningRate, double[] inputs, double[] targets) {
+        this.weights = new double[inputSize + 3][7];
         this.bias = new double[7];
-        this.out = new double[outs];
-
-        if(inputSize > inputs.length){
-            inputSize = inputs.length;
-        }
-        if(outs > targets.length){
-            outs = targets.length;
-        }
-        if(inputs.length > 0){
-            System.arraycopy(inputs, 0, this.inputs, 0, inputSize);
-        }
-
-        for (int i = 0; i < inputSize; i++) {
-            for (int j = 0; j < 7; j++) {
-                this.weights[i][j] = Math.random() * 2 - 1;
-            }
-        }
-        for (int i = 0; i < 7; i++) {
-            this.bias[i] = Math.random() * 2 - 1 * 0.1;
+        this.out = new double[7];
+        double scale = Math.sqrt(2.0 / inputSize);
+        for (int i = 0; i < this.weights.length; i++) {
+            for (int j = 0; j < 7; j++) this.weights[i][j] = (Math.random() * 2 - 1) * scale;
         }
         this.targets = targets;
         this.inputs = inputs;
@@ -42,75 +24,25 @@ public class Neuron {
     }
 
     private native static double activate(double sum);
-
     private native static double derivative(double output);
-
     public native double predict();
-
     public native void train(double target);
-
     public native void motivate(double reward, double[] nextState, double gamma);
-
-    public double[] getTarget() {
-        return targets;
-    }
-
-    public void setTarget(double[] targets) {
-        this.targets = targets;
-    }
-
-    public void setInputs(double[] inputs) {
-        this.inputs = inputs;
-    }
-
-    public void addInput(double[] input){
-        this.inputs = input;
-    }
-
-    public void addWeight(double[] weight){
-        double[][] newWeights = new double[this.weights.length + 1][];
-        System.arraycopy(this.weights, 0, newWeights, 0, this.weights.length);
-        newWeights[this.weights.length] = weight;
-        this.weights = newWeights;
-    }
-
-    public void setWeights(double[][] newWeights) {
-        for (int i = 0; i < newWeights.length; i++) {
-            for (int j = 0; j < newWeights[i].length; j++) {
-                this.weights[i][j] = Math.max(-1.0, Math.min(1.0, newWeights[i][j]));
-            }
-        }
-    }
-
-    public double[][] getWeights() {
-        return weights;
-    }
-
     public native void exportWeights(String path, double[][] weightsToExport);
-
     public native void importWeights(String path);
 
-    public void save(String fileName) {
-        for(int i = 0; i < this.weights.length; i++){
-            for(int j = 0; j < this.weights[i].length; j++){
-                if(Double.isNaN(bias[j])){
-                    this.bias[j] = 0.0;
-                }
-                if(Double.isNaN(weights[i][j]) || Double.isInfinite(weights[i][j])){
-                    this.weights[i][j] = 0.0;
-                }
-                this.weights[i][j] += this.bias[j];
-                this.weights[i][j] = Math.max(-1.0, Math.min(1.0, this.weights[i][j]));
-            }
-        }
-        exportWeights(fileName, this.weights);
+    public void addInput(double[] input) { this.inputs = input; }
+    public void addWeight(double[] weight) {
+        double[][] nW = new double[this.weights.length + 1][7];
+        System.arraycopy(this.weights, 0, nW, 0, this.weights.length);
+        nW[this.weights.length] = weight;
+        this.weights = nW;
     }
-
-    public void load(String fileName){
-        importWeights(fileName);
-    }
-
-    public double[] parsePic(String path){
-        return new double[0];
-    }
+    public void save(String f) { exportWeights(f, this.weights); }
+    public void load(String f) { importWeights(f); }
+    
+    public double[] getTarget() { return targets; }
+    public void setTarget(double[] t) { this.targets = t; }
+    public void setInputs(double[] i) { this.inputs = i; }
+    public double[][] getWeights() { return weights; }
 }
