1ea808fbcf96e270e4ce14a883ad01d29445a512
Rab7b <5myinc5@gmail.com>
2026-04-01 18:20:21 -0300

    practice

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,21 +6,30 @@ import ai.Neuron;
 
 public class Main extends JPanel implements ActionListener {
 
-    private int x1 = 100, y1 = 400;
+    private int x1 = 100, y1 = 300;
     private int size = 50;
     private Timer timer;
     
-    private int tries = load("tries.txt");
+    private int tries;
     private long startTime;
     private final int TIME_LIMIT_MS = 20000;
     private final int GOAL_X = 1500;
 
-    Neuron neuron = new Neuron(1, new double[]{0.0}, 0.01, 1);
+    private Neuron[] neurons = {
+        new Neuron(2, new double[]{0.0, 0.0}, 0.01, 1),
+        new Neuron(2, new double[]{0.0, 0.0}, 0.01, 1)
+    };
 
     public Main() {
         this.setFocusable(true);
         this.setBackground(Color.WHITE);
-        neuron.load("w.txt");
+        if(load("tries.txt") == 0){ 
+            tries = 0;
+        } else{
+            tries = load("tries.txt");
+        }
+        neurons[0].load("w_x.txt");
+        neurons[1].load("w_y.txt");
         this.startTime = System.currentTimeMillis();
 
         timer = new Timer(20, this);
@@ -43,7 +52,7 @@ public class Main extends JPanel implements ActionListener {
         g2d.drawString("Time Left: " + String.format("%.2f", timeLeft) + "s", 20, 60);
 
         g2d.setColor(new Color(46, 204, 113));
-        g2d.fillRect(GOAL_X, 0, 10, getHeight());
+        g2d.fillRect(GOAL_X, 0, 15, getHeight());
 
         g2d.setColor(new Color(52, 152, 219));
         g2d.fillRect(x1, y1, size, size);
@@ -53,54 +62,58 @@ public class Main extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        double normalizedX = x1 / (double)GOAL_X; 
-        neuron.setInputs(new double[]{ normalizedX });
+        double normX = x1 / 1600.0;
+        double normY = y1 / 600.0;
+        double[] inputs = { normX, normY };
 
-        double rawPrediction = neuron.predict(); 
-        double pixelPrediction = rawPrediction * GOAL_X;
+        neurons[0].setInputs(inputs);
+        neurons[1].setInputs(inputs);
 
-        if (pixelPrediction > x1) {
-             x1 += 3;
-        } else if (pixelPrediction < x1) {
-            x1 -= 3;
-        }
+        double predX = neurons[0].predict() * 1600.0;
+        double predY = neurons[1].predict() * 600.0;
+
+        if (predX > x1) x1 += 4; else if (predX < x1) x1 -= 4;
+        if (predY > y1) y1 += 4; else if (predY < y1) y1 -= 4;
 
         double reward = 0;
         long currentTime = System.currentTimeMillis();
         boolean reset = false;
-        
+
         if (currentTime - startTime > TIME_LIMIT_MS) {
             reward = -2.0;
-            System.out.println("TIMEOUT! Try: " + tries);
             reset = true;
-        } else if (x1 <= 0) {
-            reward = -1.0; 
+        } else if (x1 <= 0 || x1 >= 1600 || y1 <= 0 || y1 >= 600) {
+            reward = -1.0;
             reset = true;
         } else if (x1 >= GOAL_X) {
-            reward = 5.0;
+            reward = 10.0;
             System.out.println("GOAL REACHED! Try: " + tries);
             reset = true;
         } else {
-            reward = (x1 / (double)GOAL_X) * 0.1;
+            reward = (x1 / 1600.0) * 0.5;
         }
 
-        neuron.train(1.0);
-        neuron.motivate(reward, 0.9, normalizedX);
+        neurons[0].train(1.0);
+        neurons[1].train(0.5); // Target middle of Y axis
+        
+        neurons[0].motivate(reward, 0.9, normX);
+        neurons[1].motivate(reward, 0.9, normY);
 
         if (reset) {
             save("tries.txt", ++tries);
             x1 = 100;
+            y1 = 300;
             startTime = currentTime;
-            if (tries % 10 == 0){
-                 neuron.save("w.txt");
+            if (tries % 10 == 0) {
+                 neurons[0].save("w_x.txt");
+                 neurons[1].save("w_y.txt");
             }
         }
-
         repaint();
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("@AI_Playground_real");
+        JFrame frame = new JFrame("@AI_Playground_XY_Line");
         Main panel = new Main();
         frame.add(panel);
         frame.setSize(1600, 600);
@@ -110,34 +123,27 @@ public class Main extends JPanel implements ActionListener {
         frame.addWindowListener(new java.awt.event.WindowAdapter() {
             @Override
             public void windowClosing(java.awt.event.WindowEvent windowEvent) {
-                panel.neuron.save("w.txt");
+                panel.neurons[0].save("w_x.txt");
+                panel.neurons[1].save("w_y.txt");
                 save("tries.txt", panel.tries);
-                System.out.println("Weights saved. Goodbye!");
                 System.exit(0);
             }
         });
-
         frame.setVisible(true);
     }
 
     static void save(String filename, int tries) {
         try (java.io.PrintWriter out = new java.io.PrintWriter(filename)) {
             out.println(tries);
-        } catch (java.io.IOException e) {
-            e.printStackTrace();
-        }
+        } catch (java.io.IOException e) {}
     }
 
     static int load(String filename) {
         java.io.File file = new java.io.File(filename);
         if (!file.exists()) return 0;
-        try (java.util.Scanner scanner = new java.util.Scanner(file)) {
-            if (scanner.hasNextInt()) {
-                return scanner.nextInt();
-            }
-        } catch (java.io.IOException e) {
-            e.printStackTrace();
-        }
+        try (java.util.Scanner sc = new java.util.Scanner(file)) {
+            if (sc.hasNextInt()) return sc.nextInt();
+        } catch (java.io.IOException e) {}
         return 0;
     }
 }
